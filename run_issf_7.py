a5d28d614c2c7131175a516ee090bb191466772b
Jonathan Horne-Jones <Jonathan.Horne-Jones@ukaea.uk>
2025-06-30 10:21:28 +0100

    Fixed param list error

--- run_issf_7.py
+++ run_issf_7.py
@@ -28,6 +28,26 @@ if __name__ == '__main__':
     scale_therm_exp_CuCrZr=1.0
     scale_therm_exp_Cu=1.0
     scale_therm_exp_W=1.0
+
+    scale_therm_cond_CuCrZr=1.0
+    scale_therm_cond_Cu=1.0
+    scale_therm_cond_W=1.0
+
+    scale_density_CuCrZr=1.0
+    scale_density_Cu=1.0
+    scale_density_W=1.0
+
+    scale_youngs_CuCrZr=1.0
+    scale_youngs_Cu=1.0
+    scale_youngs_W=1.0
+
+    scale_spec_heat_CuCrZr=1.0
+    scale_spec_heat_Cu=1.0
+    scale_spec_heat_W=1.0
+
+    scale_poisson_CuCrZr=1.0
+    scale_poisson_Cu=1.0
+    scale_poisson_W=1.0
     
     #------------------------- END -------------------------------------------
     #-------------------------------------------------------------------------
