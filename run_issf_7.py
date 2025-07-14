5deeadd391fae913f38d94e37ed9a277ca6f57cc
Jonathan Horne-Jones <Jonathan.Horne-Jones@ukaea.uk>
2025-07-14 08:11:59 +0100

    Reduced input set based on sensitivity

--- run_issf_7.py
+++ run_issf_7.py
@@ -20,36 +20,23 @@ if __name__ == '__main__':
     inputJsonPathStr = None     # Either a path to a json file containing input parameters to update or None
     
     # Input parameters
-    coolantTemp=175             # degC
+    coolantTemp=155             # degC
     convectionHTC=150000        # W/m^2K
-    topSurfHeatFlux=2e7         # W/m^2
+
+    topSurfHeatFlux=2.5e7       # W/m^2
+
     sideSurfHeatFlux=2e8        # W/m^2
     protrusion=0.000            # m - the distance monoblock protrudes past neighbour
-    coolantPressure=5e6         # Pa
 
-    scale_therm_exp_CuCrZr=1.0
     scale_therm_exp_Cu=1.0
-    scale_therm_exp_W=1.0
 
     scale_therm_cond_CuCrZr=1.0
-    scale_therm_cond_Cu=1.0
     scale_therm_cond_W=1.0
 
-    scale_density_CuCrZr=1.0
-    scale_density_Cu=1.0
-    scale_density_W=1.0
-
     scale_youngs_CuCrZr=1.0
-    scale_youngs_Cu=1.0
     scale_youngs_W=1.0
 
-    scale_spec_heat_CuCrZr=1.0
-    scale_spec_heat_Cu=1.0
-    scale_spec_heat_W=1.0
-
-    scale_poisson_CuCrZr=1.0
     scale_poisson_Cu=1.0
-    scale_poisson_W=1.0
     
     #------------------------- END -------------------------------------------
     #-------------------------------------------------------------------------
@@ -72,26 +59,13 @@ if __name__ == '__main__':
             'convectionHTC':            convectionHTC,
             'topSurfHeatFlux':          topSurfHeatFlux,
             'sideSurfHeatFlux':         sideSurfHeatFlux,
-            'coolantPressure':          coolantPressure,
             'protrusion':               protrusion,
-            'scale_therm_exp_CuCrZr':   scale_therm_exp_CuCrZr,
             'scale_therm_exp_Cu':       scale_therm_exp_Cu,
-            'scale_therm_exp_W':        scale_therm_exp_W,
             'scale_therm_cond_CuCrZr':  scale_therm_cond_CuCrZr,
-            'scale_therm_cond_Cu':      scale_therm_cond_Cu,
             'scale_therm_cond_W':       scale_therm_cond_W,
-            'scale_spec_heat_CuCrZr':   scale_spec_heat_CuCrZr,
-            'scale_spec_heat_Cu':       scale_spec_heat_Cu,
-            'scale_spec_heat_W':        scale_spec_heat_W,
-            'scale_density_CuCrZr':     scale_density_CuCrZr,
-            'scale_density_Cu':         scale_density_Cu,
-            'scale_density_W':          scale_density_W,
             'scale_youngs_CuCrZr':      scale_youngs_CuCrZr,
-            'scale_youngs_Cu':          scale_youngs_Cu,
             'scale_youngs_W':           scale_youngs_W,
-            'scale_poisson_CuCrZr':     scale_poisson_CuCrZr,
             'scale_poisson_Cu':         scale_poisson_Cu,
-            'scale_poisson_W':          scale_poisson_W
         }
     else:
         jsonPath = Path(inputJsonPathStr)
--- run_issf_7_from_windows.py
+++ run_issf_7_from_windows.py
@@ -23,34 +23,21 @@ inputJsonPathStr = None     # EITHER a path to the JSON file OR None
 # Input parameters - ignored if using JSON input
 coolantTemp=155             # degC
 convectionHTC=150000        # W/m^2K
+
 topSurfHeatFlux=2.5e7       # W/m^2
+
 sideSurfHeatFlux=2e8        # W/m^2
 protrusion=0.000           # m - the distance monoblock protrudes past neighbour
-coolantPressure=5e6         # Pa
 
-scale_therm_exp_CuCrZr=1.2
-scale_therm_exp_Cu=1.1
-scale_therm_exp_W=1.0
+scale_therm_exp_Cu=1.0
 
-scale_therm_cond_CuCrZr=1.2
-scale_therm_cond_Cu=1.0
+scale_therm_cond_CuCrZr=1.0
 scale_therm_cond_W=1.0
 
-scale_spec_heat_CuCrZr=1.0
-scale_spec_heat_Cu=1.05
-scale_spec_heat_W=1.0
-
-scale_density_CuCrZr=1.0
-scale_density_Cu=1.0
-scale_density_W=1.0
-
 scale_youngs_CuCrZr=1.0
-scale_youngs_Cu=1.0
 scale_youngs_W=1.0
 
-scale_poisson_CuCrZr=1.0
 scale_poisson_Cu=1.0
-scale_poisson_W=1.0
 
 #------------------------- END -------------------------------------------
 #-------------------------------------------------------------------------
@@ -62,26 +49,13 @@ if inputJsonPathStr is None:
         'convectionHTC':            convectionHTC,
         'topSurfHeatFlux':          topSurfHeatFlux,
         'sideSurfHeatFlux':         sideSurfHeatFlux,
-        'coolantPressure':          coolantPressure,
         'protrusion':               protrusion,
-        'scale_therm_exp_CuCrZr':   scale_therm_exp_CuCrZr,
         'scale_therm_exp_Cu':       scale_therm_exp_Cu,
-        'scale_therm_exp_W':        scale_therm_exp_W,
         'scale_therm_cond_CuCrZr':  scale_therm_cond_CuCrZr,
-        'scale_therm_cond_Cu':      scale_therm_cond_Cu,
         'scale_therm_cond_W':       scale_therm_cond_W,
-        'scale_spec_heat_CuCrZr':   scale_spec_heat_CuCrZr,
-        'scale_spec_heat_Cu':       scale_spec_heat_Cu,
-        'scale_spec_heat_W':        scale_spec_heat_W,
-        'scale_density_CuCrZr':     scale_density_CuCrZr,
-        'scale_density_Cu':         scale_density_Cu,
-        'scale_density_W':          scale_density_W,
         'scale_youngs_CuCrZr':      scale_youngs_CuCrZr,
-        'scale_youngs_Cu':          scale_youngs_Cu,
         'scale_youngs_W':           scale_youngs_W,
-        'scale_poisson_CuCrZr':     scale_poisson_CuCrZr,
         'scale_poisson_Cu':         scale_poisson_Cu,
-        'scale_poisson_W':          scale_poisson_W
     }
     
     # Write dictionary to json
